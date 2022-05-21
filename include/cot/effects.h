1f4ad3408544e112fb7f75b44f1a1deb1342c110
Parakoopa <parakoopa@live.de>
2022-05-21 19:08:17 +0200

    Restructure c-of-time's effect logic to be easier to use for C-devs

--- include/cot/effects.h
+++ include/cot/effects.h
@@ -5,3 +5,8 @@ typedef struct move_effect_input {
   int item_id;
   bool out_dealt_damage;
 } move_effect_input;
+
+// Custom effects handling functions.
+bool CustomApplyItemEffect(struct entity* user, struct entity* target, struct item* item, bool is_thrown);
+bool CustomApplyMoveEffect(move_effect_input* data, struct entity* user, struct entity* target, struct move* move);
+bool CustomScriptSpecialProcessCall(undefined4* unknown, uint32_t special_process_id, short arg1, short arg2, int* return_val);
