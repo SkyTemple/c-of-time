8beeeba28adc4ffa0beb38a83a42d853c68d87d0
Parakoopa <parakoopa@live.de>
2022-05-21 18:13:18 +0200

    Review feedback: Better file read/seek

--- rust/eos-rs/src/api/io.rs
+++ rust/eos-rs/src/api/io.rs
@@ -19,6 +19,12 @@ pub mod file {
     use core::ffi::CStr;
     use core::mem::MaybeUninit;
 
+    /// This counter mutex is safe to access by methods of this module, since the NDS is
+    /// single-threaded. There are probably some reasons regarding missing atomic support and the
+    /// ARM instruction set due to which this isn't true if for example interrupts happen, but
+    /// this is marked in the safety note of [`read`] and [`File::open`].
+    static mut COUNT_IN_FILE_TRANSFER_MODE: usize = 0;
+
     /// Loads a file from ROM by filepath into a heap-allocated buffer.
     ///
     /// # Safety
@@ -26,11 +32,17 @@ pub mod file {
     ///
     /// Additionally, see safety note for [`Vec::from_raw_parts`]. It's probably safer to
     /// use the [`File`] struct instead.
+    ///
+    /// You must make sure this is not called during interrupts.
     pub unsafe fn read<C: AsRef<CStr>>(path: C, flags: u32) -> Vec<u8> {
         let mut iov_raw = MaybeUninit::uninit();
-        ffi::DataTransferInit();
+        if COUNT_IN_FILE_TRANSFER_MODE == 0 {
+            ffi::DataTransferInit();
+        }
         ffi::LoadFileFromRom(iov_raw.as_mut_ptr(), path.as_ref().as_ptr(), flags);
-        ffi::DataTransferStop();
+        if COUNT_IN_FILE_TRANSFER_MODE == 0 {
+            ffi::DataTransferStop();
+        }
         let iov = iov_raw.assume_init();
         Vec::from_raw_parts(
             iov.iov_base as *mut u8,
@@ -39,6 +51,7 @@ pub mod file {
         )
     }
 
+    /// A file in the NDS file-system.
     pub struct File(ffi::file_stream);
 
     impl File {
@@ -47,13 +60,28 @@ pub mod file {
         ///
         /// # Safety
         /// The file path must be a valid path to an existing file in the ROM file system.
+        ///
+        /// You must make sure the file object or readers made for it are never accessed from code
+        /// during interrupts.
         pub unsafe fn open<C: AsRef<CStr>>(path: C) -> Self {
             let mut file_stream = MaybeUninit::uninit();
-            ffi::DataTransferInit();
+            if COUNT_IN_FILE_TRANSFER_MODE == 0 {
+                ffi::DataTransferInit();
+            }
             ffi::FileInit(file_stream.as_mut_ptr());
             ffi::FileOpen(file_stream.as_mut_ptr(), path.as_ref().as_ptr());
-            ffi::DataTransferStop();
+            if COUNT_IN_FILE_TRANSFER_MODE == 0 {
+                ffi::DataTransferStop();
+            }
             Self(file_stream.assume_init())
+        }
+
+        /// Returns a reader for the file (implements `Read` and `Seek` for it).
+        ///
+        /// During the lifetime of the reader the NDS will be put in file-transfer mode, if it isn't
+        /// already.
+        pub fn reader(&mut self) -> FileReader {
+            FileReader::new(self)
         }
 
         /// Gets the size of the open file.
@@ -67,36 +95,65 @@ pub mod file {
         }
     }
 
-    impl Read for File {
+    /// A reader for a [`File`].
+    ///
+    /// During the lifetime of the reader the NDS will be put in file-transfer mode, if it isn't
+    /// already.
+    pub struct FileReader<'a>(&'a mut File);
+
+    impl<'a> FileReader<'a> {
+        pub fn new(file: &'a mut File) -> Self {
+            unsafe {
+                if COUNT_IN_FILE_TRANSFER_MODE == 0 {
+                    ffi::DataTransferInit();
+                }
+                COUNT_IN_FILE_TRANSFER_MODE += 1;
+                Self(file)
+            }
+        }
+    }
+
+    impl<'a> Drop for FileReader<'a> {
+        fn drop(&mut self) {
+            unsafe {
+                // This can only be false if this has somehow gotten out of sync, but let's just be
+                // safe here...
+                if COUNT_IN_FILE_TRANSFER_MODE > 0 {
+                    COUNT_IN_FILE_TRANSFER_MODE -= 1;
+                }
+                if COUNT_IN_FILE_TRANSFER_MODE == 0 {
+                    ffi::DataTransferStop();
+                }
+            }
+        }
+    }
+
+    impl<'a> Read for FileReader<'a> {
         fn read(&mut self, dst: &mut [u8]) -> super::Result<usize> {
             unsafe {
-                ffi::DataTransferInit();
                 let len = ffi::FileRead(
-                    &mut self.0,
+                    &mut self.0.0,
                     dst.as_mut_ptr() as *mut c_void,
                     dst.len() as u32,
                 ) as usize;
-                ffi::DataTransferStop();
                 Ok(len)
             }
         }
     }
 
-    impl Seek for File {
+    impl<'a> Seek for FileReader<'a> {
         /// Seeking from End might not be properly implemented in the game.
         ///
         /// Additionally the position must fit into an i32.
         fn seek(&mut self, pos: SeekFrom) -> super::Result<u64> {
             unsafe {
-                ffi::DataTransferInit();
                 match pos {
-                    SeekFrom::Start(p) => ffi::FileSeek(&mut self.0, p as i32, 0),
-                    SeekFrom::Current(p) => ffi::FileSeek(&mut self.0, p as i32, 1),
-                    SeekFrom::End(p) => ffi::FileSeek(&mut self.0, p as i32, 2),
+                    SeekFrom::Start(p) => ffi::FileSeek(&mut self.0.0, p as i32, 0),
+                    SeekFrom::Current(p) => ffi::FileSeek(&mut self.0.0, p as i32, 1),
+                    SeekFrom::End(p) => ffi::FileSeek(&mut self.0.0, p as i32, 2),
                 }
-                ffi::DataTransferStop();
 
-                Ok((self.0.current_address as u64) - (self.0.start_address as u64))
+                Ok((self.0.0.current_address as u64) - (self.0.0.start_address as u64))
             }
         }
     }
