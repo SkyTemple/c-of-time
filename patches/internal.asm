8a1af65e4fc8d672ce458e690ab42b88c738403b
Adex <91418697+Adex-8x@users.noreply.github.com>
2024-12-24 18:22:50 -0500

    Add template for custom script menus (#264)
    * Add template for custom script menus
    
    * Clarifying comments
    
    * Use ifdef instead of comments
    
    * Explicit `void` arguments
    
    ---------
    
    Co-authored-by: tech-ticks <techticksdev@gmail.com>

--- patches/internal.asm
+++ patches/internal.asm
@@ -1,17 +1,42 @@
 .nds
 .include "symbols.asm"
 
+.open "arm9.bin", arm9_start
+  .ifdef HookScriptMenuRequestCheck
+  .org ShowKeyboard
+    push {r3-r8,lr}
+    bl HookKeyboardCheck
+
+  .org ShowKeyboardTypeCase3
+    b HookKeyboardCustomPrompt
+
+  .org ShowKeyboardReturn
+    pop {r3-r8,pc}
+
+  .org PreprocessStringFromIdCallsite
+    bl CustomPreprocessStringFromId
+  .endif
+.close
+
 .open "overlay11.bin", overlay11_start
   .org ScriptSpecialProcessCall
     b cotInternalTrampolineScriptSpecialProcessCall
 
-  // Remove the comments below to enable custom instructions (see `ground_instructions.c`).
+  .ifdef HookScriptMenuRequestCheck
+  .org ScriptMenuRequestDefaultCase
+    b HookScriptMenuRequestCheck
+  
+  .org ScriptMenuUpdateDefaultCase
+    b HookScriptMenuUpdateCheck
+  .endif
 
-  // .org OpcodeCheck
-  //   b HookOpcodeCheck
-  //
-  // .org GetParameterCount
-  //   bl HookGetParameterCount
+  .ifdef HookOpcodeCheck
+  .org OpcodeCheck
+    b HookOpcodeCheck
+  
+  .org GetParameterCount
+    bl HookGetParameterCount
+  .endif
 .close 
 
 .open "overlay29.bin", overlay29_start
